482ea2e87a82fa5a6fe85e6dc7ec1050e188e252
Shkodyak Andrey <fluxuator@gmail.com>
2011-11-27 03:54:10 +0200

    #1 Исправлена несовместимость с aceAdminPanel

--- PluginDebugtoolbar.class.php
+++ PluginDebugtoolbar.class.php
@@ -14,102 +14,109 @@
 
 // Prevents a direct access to the file
 if (!class_exists('Plugin')) {
-    die('Hacking attemp!');
+	die('Hacking attemp!');
 }
 
 class PluginDebugtoolbar extends Plugin
 {
-  
-    // SQL queries storage
-    protected static $aSqlData = array();
 
-    /**
-     * Store SQL data into temp storage
-     *
-     * @param object $oDb
-     * @param string $sMessage
-     */
-    public static function setSqlData($oDb, $sMessage)
-    {
-        static $iQueryCount = 0;
+	// SQL queries storage
+	protected static $aSqlData = array();
 
-        /**
-         * Так как пока не получается нормально получить время исполнения запроса и сам запрос,
-         * будем парсить кашу которая стекается в лог файл.
-         * @TODO: Найти способ нормально получить данные о запросах из DbSimple.
-         */
-        if (false !== strpos($sMessage, '--')) {
-            // This is result: -- 0 ms; returned 0 row(s)
-            if (preg_match("/(\d+).+returned\s+(.*)/u", $sMessage, $aMatch)) {
-                self::$aSqlData[$iQueryCount]['time'] = $aMatch[1];
-                self::$aSqlData[$iQueryCount]['return'] = $aMatch[2];
-            } else {
-                self::$aSqlData[$iQueryCount]['time'] = 0;
-                self::$aSqlData[$iQueryCount]['return'] = '';
-            }
-        } else {
-            // This is query. So let's clean it to pretty view
+	/**
+	 * Store SQL data into temp storage
+	 *
+	 * @param object $oDb
+	 * @param string $sMessage
+	 */
+	public static function setSqlData($oDb, $sMessage)
+	{
+		static $iQueryCount = 0;
 
-            $aReplace = array(
-                "#\s+#u" => ' ',
-                '#[,.]#u' => '\1 '
-            );
+		/**
+		 * Так как пока не получается нормально получить время исполнения запроса и сам запрос,
+		 * будем парсить кашу которая стекается в лог файл.
+		 * @TODO: Найти способ нормально получить данные о запросах из DbSimple.
+		 */
+		if (false !== strpos($sMessage, '--')) {
+			// This is result: -- 0 ms; returned 0 row(s)
+			if (preg_match("/(\d+).+returned\s+(.*)/u", $sMessage, $aMatch)) {
+				self::$aSqlData[$iQueryCount]['time'] = $aMatch[1];
+				self::$aSqlData[$iQueryCount]['return'] = $aMatch[2];
+			} else {
+				self::$aSqlData[$iQueryCount]['time'] = 0;
+				self::$aSqlData[$iQueryCount]['return'] = '';
+			}
+		} else {
+			// This is query. So let's clean it to pretty view
 
-            $sMessage = preg_replace(array_keys($aReplace), array_values($aReplace), $sMessage);
+			$aReplace = array(
+				"#\s+#u" => ' ',
+				'#[,.]#u' => '\1 '
+			);
 
-            $iQueryCount++;
-            self::$aSqlData[$iQueryCount]['query'] = trim($sMessage);
-        }
-    }
+			$sMessage = preg_replace(array_keys($aReplace), array_values($aReplace), $sMessage);
 
-    /**
-     * Get SQL data from temp storage
-     *
-     * @return array
-     */
-    public static function getSqlData()
-    {
-        return self::$aSqlData;
-    }
+			$iQueryCount++;
+			self::$aSqlData[$iQueryCount]['query'] = trim($sMessage);
+		}
+	}
 
-    /**
-     * Указанные в массивы наследования будут переданы движку автоматически
-     * перед инициализацией плагина
-     */
-    public $aInherits = array(
-        'module' => array('ModuleViewer')
-    );
+	/**
+	 * Get SQL data from temp storage
+	 *
+	 * @return array
+	 */
+	public static function getSqlData()
+	{
+		return self::$aSqlData;
+	}
 
-    /**
-     * Конструктор плагина
-     */
-    public function __construct()
-    {
+	/**
+	 * Конструктор плагина
+	 */
+	public function __construct()
+	{
 
-        parent::__construct();
+		parent::__construct();
 
-        // Включаем логирование запросов, для того чтобы их позже вывести в панель
-        Engine::getInstance()->Database_GetConnect()->setLogger('PluginDebugtoolbar::setSqlData');
-    }
+		// Включаем логирование запросов, для того чтобы их позже вывести в панель
+		Engine::getInstance()->Database_GetConnect()->setLogger('PluginDebugtoolbar::setSqlData');
 
-    /**
-     * Plugin activation
-     *
-     * @return boolean
-     */
-    public function Activate()
-    {
-        $this->Cache_Clean();
-        return true;
-    }
+		// Активируем настройки шаблонизатора для нужд плагина
+		$this->SetupSmarty();
+		
+	}
 
-    /**
-     * Plugin initialization
-     */
-    public function Init()
-    {
-        
-    }
+	/**
+	 * Plugin activation
+	 *
+	 * @return boolean
+	 */
+	public function Activate()
+	{
+		$this->Cache_Clean();
+		return true;
+	}
+
+	/**
+	 * Plugin initialization
+	 */
+	public function Init()
+	{
+		
+	}
+
+	/**
+	 * Устанавливает собственные настройки для шаблонизатора
+	 */
+	protected function SetupSmarty()
+	{
+		$oSmarty = $this->Viewer_GetSmartyObject();
+		
+		// Переопределяем шаблон отладчика Smarty
+		$oSmarty->debug_tpl = Plugin::GetTemplatePath(__CLASS__) . 'smarty.debug.tpl';
+	}
 
 }
 
